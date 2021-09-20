47f1f192b6e90c87e63ff1c1d5a2cf1f2cbeeffe
NRITTIK <nrittik43@gmail.com>
2021-09-20 11:16:44 +0530

    Completed MiniProject-1

--- MiniProject1/code.c
+++ MiniProject1/code.c
@@ -1,42 +1,65 @@
 #include <stdio.h>
-#include <stdlib.h>
-// #include <string.h>
 
-int strlen(char* str){
-    // char* ptr = str;
-    int len=0;
-    while(str[len]!='\0'){
-        len++;
+#define MAX 4200
+
+void strSum(char *res, char *str1, char *str2)
+{
+    int carry = 0;
+    for (int i = MAX - 1; i >= 0; i--)
+    {
+        int digit = (str1[i] - '0') + (str2[i] - '0') + carry;
+        res[i] = (digit % 10) + '0';
+        carry = digit / 10;
     }
-    return len;
 }
 
-void strSum(char* res, char* str1, char *str2){
-    
+void strcopy(char *str1, char *str2)
+{
+    for (int i = 0; i < MAX; ++i)
+    {
+        str2[i] = str1[i];
+    }
 }
 
-void fibonacci(int n){
-    char str1[100], str2[100], res[100];
-    if(n<=1){
-        sprintf(res, "%d", n);
-        // return str1;
+int fibonacci(int n)
+{
+    char a[MAX], b[MAX], c[MAX];
+    for(int i=0; i<MAX; ++i){
+        a[i]=b[i]=c[i]='0';
     }
-    else{
-        for(int i=2; i<=n; ++i){
-            sprintf(str1, "%d", n-1);
-            sprintf(str2, "%d", n-2);
-            strSum(res, str1, str2);
+    b[MAX-1] = '1';
+    int i;
+    if (n < 2)
+    {
+        printf("%c", n+'0');
+    }
+    else
+    {
+        for (i = 2; i <= n; ++i)
+        {
+            strSum(c, a, b);
+            strcopy(b, a);
+            strcopy(c, b);
+        }
+        int firstInt=0;
+        for(i=0; i<MAX; ++i){
+            if(b[i]!='0'){
+                break;
+            }
+            firstInt++;
+        }
+        for(i=firstInt; i<MAX; ++i){
+            printf("%c", b[i]);
         }
     }
-    
 }
 
-int main(){
-    // int n;
-    // scanf("%d", &n);
+int main()
+{
+    int n;
+    scanf("%d", &n);
 
-    // int value = fibonacci(n);
-    // printf("%dth fibonacci number is: %llu", n, value);
-    
+    fibonacci(n);
+
     return 0;
 }
