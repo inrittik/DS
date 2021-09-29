053067d6d63b62486665c42d7334c0a7f5230854
NRITTIK <nrittik43@gmail.com>
2021-09-29 14:22:09 +0530

    assignment 1 question 3

---
+++ Assignment1/ques3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void convertTo2D(int arr[], int rows, int cols){
+    int k=0;
+    int *res;
+    for(int i=0; i<rows; ++i){
+        for(int j=0; j<cols; ++j){
+            printf("%d ", arr[i*cols+j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int rows, cols;
+    scanf("%d %d", &rows, &cols);
+    int arr[rows*cols];
+    for(int i=0; i<rows*cols; ++i){
+        scanf("%d", &arr[i]);
+    }
+    printf("\n");
+    convertTo2D(arr, rows, cols);
+    return 0;
+}
